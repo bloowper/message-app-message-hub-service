2377d39847ddd5be91ae6e7b505e4102391179a0
Tomasz Orchowski <tomasz.orchowski@codibly.com>
2022-08-06 14:26:45 +0200

    service instance uuid log

--- src/main/java/orchowski/tomasz/message_hub/configuration/ServiceConfiguration.java
+++ src/main/java/orchowski/tomasz/message_hub/configuration/ServiceConfiguration.java
@@ -1,6 +1,7 @@
 package orchowski.tomasz.message_hub.configuration;
 
 
+import lombok.extern.slf4j.Slf4j;
 import orchowski.tomasz.message_hub.configuration.dto.ServiceUuidDto;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -8,12 +9,15 @@ import org.springframework.context.annotation.Configuration;
 import java.util.UUID;
 
 @Configuration
+@Slf4j
 class ServiceConfiguration {
     /**
      * @return uuid of service instance
      */
     @Bean
     ServiceUuidDto serviceUuidDto() {
-        return new ServiceUuidDto(UUID.randomUUID().toString());
+        ServiceUuidDto serviceUuidDto = new ServiceUuidDto(UUID.randomUUID().toString());
+        log.info("Service instance uuid [{}]", serviceUuidDto.uuid());
+        return serviceUuidDto;
     }
 }
