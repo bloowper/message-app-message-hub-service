28820ceceb8733bb066963f1ae15d60d0d811e18
Tomasz Orchowski <gtatomek@gmail.com>
2022-11-13 00:40:41 +0100

    .

--- src/test/java/orchowski/tomasz/message_hub/messagechoreographer/MessageChoreographerFacadeIT.java
+++ src/test/java/orchowski/tomasz/message_hub/messagechoreographer/MessageChoreographerFacadeIT.java
@@ -1,10 +1,16 @@
 package orchowski.tomasz.message_hub.messagechoreographer;
 
 
+import orchowski.tomasz.message_hub.shared.InfrastructureTestContainer;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class MessageChoreographerFacadeIT {
+class MessageChoreographerFacadeIT extends InfrastructureTestContainer {
 
+    @Test
+    void usersInSameChannelShouldCommunicate() {
+        // TODO
+    }
 
 }
